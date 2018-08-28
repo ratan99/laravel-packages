5918d0640cfc15a6ef64a280f7a6155e132fd7d6
root <root@MSI.localdomain>
2018-08-28 13:42:12 +0530

    init

---
+++ packages/tpt/gamify/src/GamifyServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace Tpt\Gamify;
+
+use Illuminate\Support\ServiceProvider;
+
+class GamifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
